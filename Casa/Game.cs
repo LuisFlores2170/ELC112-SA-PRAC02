4dca00360a1c6382e9b0e5b0ad02327261eda7c1
LuisFlores2170 <hinojosafloresluisfernando@gmail.com>
2023-04-08 03:21:30 -0400

    update

---
+++ Casa/Game.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+
+namespace Casa
+{
+    public class Game : GameWindow
+    {
+        float largo = 100;
+        float alto = 80;
+        float ancho = 200;
+       public Game(int width, int height, string title) : base(width, height, GraphicsMode.Default, title)
+        {
+
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            int oSize = 300;
+            GL.ClearColor(Color4.Black);
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadIdentity();
+            GL.Ortho(-oSize, oSize, -oSize, oSize, -oSize, oSize);
+            base.OnLoad(e);
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            GL.Enable(EnableCap.DepthTest);
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadIdentity();
+            GL.Rotate(150, 0, 1, 0);
+            
+
+            Home home = new Home(new Vector3(0, 0, 0), largo, alto, ancho);
+            home.draw();
+            SwapBuffers();
+            base.OnRenderFrame(e);
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            //GL.Rotate(1, 0.0f, 1f, 0.0f);
+            base.OnUpdateFrame(e);
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            GL.Viewport(0, 0, Width, Height);
+            base.OnResize(e);
+        }
+    }
+}
---
+++ Casa/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenTK;
+
+namespace Casa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GameWindow window = new GameWindow(800, 600);
+            Game game = new Game(800, 600, "Drawing a House");
+            game.Run(60.0);
+        }
+    }
+}
